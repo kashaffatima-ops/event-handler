71f1e28411bd4a2340d0c809400f719061d1917e
kashaffatima-ops <i222415@nu.edu.pk>
2025-03-13 09:32:22 +0500

    third commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,6 +6,7 @@ exports.register = async (req, res) => {
     const { name, email, password } = req.body;
     try {
         const user = new User({ name, email, password });
+        console.log("Check for lab task");
         await user.save();
         res.status(201).json({ message: 'User registered successfully' });
     } catch (err) {
