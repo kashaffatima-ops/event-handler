0b3b78eda21a5557bd8058313bbd9b6f05865980
kashaffatima-ops <i222415@nu.edu.pk>
2025-03-13 09:39:52 +0500

    sixth commit

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -4,7 +4,7 @@ exports.createEvent = async (req, res) => {
     try {
         const event = new Event({ ...req.body, userId: req.user.id });
         await event.save();
-        console.log("Check for lab task");
+        console.log("Check for lab task 8");
         res.status(201).json(event);
     } catch (err) {
         res.status(400).json({ error: err.message });
